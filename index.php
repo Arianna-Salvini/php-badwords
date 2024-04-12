a615899fd5ce4f7fd7a3b1a18364e3b233c1f091
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-12 16:46:24 +0200

    Add form for text and for word to be censored in index file

--- index.php
+++ index.php
@@ -6,6 +6,20 @@
     <title>Badwords</title>
 </head>
 <body>
+    <!-- creare il form per paragrafo e parola da censurare -->
+    <div class="form_container">
+        <h1> Badword censor</h1>
+        <form action="" method="post">
+            <div class="text_area">
+                <textarea name="paragraph" id="paragraph" cols="50" rows="10" placeholder="Inserisci il tuo testo: "></textarea>
+            </div>
+            <div class="badword_censor_section">
+                <label for="badword">Inserisci la parola da censurare</label>
+                <input type="text" name="badword" id="badword">
+                <input type="submit" value="Invia">
+            </div>
+        </form>
+    </div>
     
 </body>
 </html>
