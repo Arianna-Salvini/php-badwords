66c55377ea018be498755ceca0fcad980f4cfef6
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-16 16:36:01 +0200

    Fix bugs

--- script.php
+++ script.php
@@ -1,34 +1,49 @@
 <?php
 //ricevere i dati
-$textParagraph =$_POST['paragraph'];
+$textParagraph = $_POST['paragraph'];
 $badword = $_POST['badword'];
 
 var_dump($textParagraph);
 var_dump($badword);
 
 $textCensored = str_replace($badword, '***', $textParagraph);
+
+
+echo '<pre>';
+
+var_dump($textCensored);
+
+echo '</pre>';
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Badwords</title>
 </head>
+
 <body>
 
-<!-- stampare a schermo il paragrafo e la sua lunghezza -->
+    <!-- stampare a schermo il paragrafo e la sua lunghezza -->
 
     <h3>Il tuo testo</h3>
     <!-- Stampare il testo censurato -->
-    <!-- <p> <?= $textParagraph?> </p> -->
-    <p> <?= $textCensored?> </p>
+    <!-- <p> <?= $textParagraph ?> </p> -->
+    <p> <?= $textCensored ?> </p>
 
     <div class="parag_length">
-        <?= strlen($textParagraph) ?> 
+        <?= strlen($textParagraph) ?>
     </div>
+
+    <button>
+        <a href="./index.php">Back</a>
+    </button>
 
 
 </body>
+
 </html>
